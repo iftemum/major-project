5185f0f0f28c71457084e953ff3a7aa695825022
iftemum <iftemumraian@gmail.com>
2020-10-13 11:37:26 -0600

    making of the player character

---
+++ Major Project Folder/proposal.md
@@ -0,0 +1,26 @@
+# Project Proposal
+
+## Description
+
+
+## Need to have list
+- The Playing Character
+
+- Enemy Spaceships
+
+- Bullets from enemies
+
+- Bullets from player
+
+- A Starting Screen
+
+- A Scoreboard
+
+- A Space background
+
+## Nice to have list
+
+- High Score menu
+
+- equippable upgrades in game
+
---
+++ Major Project Folder/sketch.js
@@ -0,0 +1,27 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+}
+
+class PlayerShip {
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.dy = 
+  }
+  
+
+}
