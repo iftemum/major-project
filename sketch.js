064adf30eb3646744e99386d9acdd604f4117268
iftemum <iftemumraian@gmail.com>
2020-10-20 11:50:36 -0600

    creating enemies as a class

--- sketch.js
+++ sketch.js
@@ -5,38 +5,45 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let spaceBackground;
+let alien;
 let score = 0;
 let lives = 3;
-let player = {
-  x: 400,
-  y: 700,
-  width: 60,
-  dx: 4,
+let raian;
 
-}
+let player = {
+  x: 200,
+  y: 520,
+  width: 50,
+  dx: 7,
+};
 
 function preload(){
   spaceBackground = loadImage("assets/space.jpg");
+  alien = loadImage("assets/alien.png");
 }
 
 function setup() {
   createCanvas(800, 800);
+  raian = new Enemy(100, 100);
 
 }
 
 function draw() {
   background("black");
-  createEnemy();
   displayUI();
   displayPlayer();
   playerMovement();
+  raian.move();
+  raian.display();
+  
 }
 
 
 function displayUI() {
   fill(255,255,255);
-  stroke(50,150,50)
-  strokeWeight(8);
+  stroke(50,150,50);
+  strokeWeight(4);
   textSize(30);
   textAlign("LEFT");
   text("SCORE : " + score, 20, 45);
@@ -47,45 +54,60 @@ function displayUI() {
 
 
 function displayPlayer(){
-  fill(80,255,75);
+  fill("green");
   noStroke();
   rectMode(CENTER);
-  rect(player.x, player.y, player.width, 30);
-  // rect(player.x, player.y-15, 15, 30);
+  rect(player.x, player.y, player.width, 20);
+  rect(player.x, player.y-15, 15, 30);
+
 
 
   
 }
 
-function createEnemy(){
-  for (let i = 0; i <= 5; i++){
-    for (let j = 0; j<=3 ; j++){
-      
-      square(i*50+50, j*50+77, 25);
-
-      
-      
-      
-    }
+class Enemy {
+  constructor(x, y){
+    this.x = x;
+    this.y = y; 
+    
   }
+
+  move(){
+    for(let i = 0; i<width; i++){
+      this.x += 3;
+    }
+    if (this.x>width-player.width){
+      this.y += 10;
+    }
+
+
+  }
+
+  display(){
+    image(alien, this.x, this.y, 50, 50);
+
+  }
+
+
 }
 
 
 
 function playerMovement(){
   if (keyIsDown(LEFT_ARROW)){
-    player.x  -= player.speed;
+    player.x  -= player.dx;
 
   }
   
   else if (keyIsDown(RIGHT_ARROW)){
-    player.x += player.speed;
+    player.x += player.dx;
 
   }
   if (player.x-player.width/2< 0){
     player.x = player.width/2;
   }
-  else if (player.s + player.width/2 > width){
+  else if (player.x + player.width/2 > width){
     player.x = width - player.width/2;
   }
 }
+
