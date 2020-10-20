90988fb5278d7f6559878b79a21bc07942d17867
iftemum <58191527+iftemum@users.noreply.github.com>
2020-10-20 02:57:35 -0600

    Player movement and UI

--- Major Project Folder/sketch.js
+++ Major Project Folder/sketch.js
@@ -4,57 +4,64 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let planeX;
-let playerShip; 
-let spaceShip;
 
-function setup() {
-  createCanvas(800, windowHeight);
-}
-
-function draw() {
-  background(220);
-  createEnemy();
-  spaceShip = new Player();
+let score = 0;
+let lives = 3;
+let player = {
+  x: 400,
+  y: 700,
+  width: 60,
+  dx: 4,
 
 }
 
 function preload(){
-  playerShip = loadImage("assets/ship.jpg");
+  spaceBackground = loadImage("assets/space.jpg");
 }
 
-class Player {
-  constructor(x){
-    this.x = x;
-    this.y = 0;
+function setup() {
+  createCanvas(800, 800);
+
+}
+
+function draw() {
+  background("black");
+  createEnemy();
+  displayUI();
+  displayPlayer();
+  playerMovement();
+}
+
+
+function displayUI() {
+  fill(255,255,255);
+  stroke(50,150,50)
+  strokeWeight(8);
+  textSize(30);
+  textAlign("LEFT");
+  text("SCORE : " + score, 20, 45);
+  textAlign("RIGHT");
+  text("LIVES : " + lives , 600, 45);
+
+}
+
+
+function displayPlayer(){
+  fill(80,255,75);
+  noStroke();
+  rectMode(CENTER);
+  rect(player.x, player.y, player.width, 30);
+  // rect(player.x, player.y-15, 15, 30);
+
+
   
-  }
-
-  move() {
-    if (keyIsDown(26)) {
-      this.x += 5;
-    }
-    if (keyIsDown(27)){
-      this.x -= 5;
-
-    }
-
-  }
-
-  display() {
-    image(playerShip, this.x, 0 + 50, 150, 150);
-  }
-  
-
-
-
 }
 
 function createEnemy(){
   for (let i = 0; i <= 5; i++){
     for (let j = 0; j<=3 ; j++){
       
-      square(i*50+50, j*50+50, 25);
+      square(i*50+50, j*50+77, 25);
 
       
       
@@ -64,4 +71,21 @@ function createEnemy(){
 }
 
 
-function 
+
+function playerMovement(){
+  if (keyIsDown(LEFT_ARROW)){
+    player.x  -= player.speed;
+
+  }
+  
+  else if (keyIsDown(RIGHT_ARROW)){
+    player.x += player.speed;
+
+  }
+  if (player.x-player.width/2< 0){
+    player.x = player.width/2;
+  }
+  else if (player.s + player.width/2 > width){
+    player.x = width - player.width/2;
+  }
+}
