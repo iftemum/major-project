7db4c5183d467860e48d10d5ee9b3532f6380fec
iftemum <iftemumraian@gmail.com>
2020-10-20 10:49:15 -0600

    reorganized

--- proposal.md
+++ proposal.md
@@ -3,6 +3,32 @@
 ## Description
 
 
+
 ## Need to have list
+- The Playing Character
+
+- Enemy Spaceships
+
+- Bullets from player
+
+- Enemies will have hit boxes and disappear when hit by a bullet
+
+- A Space background
+
+- Move horizontally with help of keys
 
 ## Nice to have list
+
+- A Starting Screen
+
+- enemies that will come in different waves
+
+- High Score menu
+
+- equippable upgrades in game
+
+- increasing difficulty
+
+- A Scoreboard
+
+- Sound
--- sketch.js
+++ sketch.js
@@ -5,25 +5,87 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let shouldIShowTheEllipse = false;
+let score = 0;
+let lives = 3;
+let player = {
+  x: 400,
+  y: 700,
+  width: 60,
+  dx: 4,
+
+}
+
+function preload(){
+  spaceBackground = loadImage("assets/space.jpg");
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(800, 800);
+
 }
 
 function draw() {
-  background(220);
+  background("black");
+  createEnemy();
+  displayUI();
+  displayPlayer();
+  playerMovement();
+}
 
-  let something = true;
 
-  if(shouldIShowTheEllipse){
+function displayUI() {
+  fill(255,255,255);
+  stroke(50,150,50)
+  strokeWeight(8);
+  textSize(30);
+  textAlign("LEFT");
+  text("SCORE : " + score, 20, 45);
+  textAlign("RIGHT");
+  text("LIVES : " + lives , 600, 45);
+
+}
+
+
+function displayPlayer(){
+  fill(80,255,75);
+  noStroke();
+  rectMode(CENTER);
+  rect(player.x, player.y, player.width, 30);
+  // rect(player.x, player.y-15, 15, 30);
+
+
   
-  fill("Red")
-  ellipse(mouseX, mouseY , 250, 250)
-    
-}
 }
 
-function mousePressed(){
-  shouldIShowTheEllipse = true;
+function createEnemy(){
+  for (let i = 0; i <= 5; i++){
+    for (let j = 0; j<=3 ; j++){
+      
+      square(i*50+50, j*50+77, 25);
+
+      
+      
+      
+    }
+  }
+}
+
+
+
+function playerMovement(){
+  if (keyIsDown(LEFT_ARROW)){
+    player.x  -= player.speed;
+
+  }
+  
+  else if (keyIsDown(RIGHT_ARROW)){
+    player.x += player.speed;
+
+  }
+  if (player.x-player.width/2< 0){
+    player.x = player.width/2;
+  }
+  else if (player.s + player.width/2 > width){
+    player.x = width - player.width/2;
+  }
 }
